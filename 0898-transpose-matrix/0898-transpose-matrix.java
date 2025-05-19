5580074a4f096454c5672b11fba6e083d2811160
adarshh1211 <153364135+adarshh1211@users.noreply.github.com>
2025-05-19 10:47:44 +0530

    Time: 0 ms (100%), Space: 44.8 MB (85.94%) - LeetHub

--- 0898-transpose-matrix/0898-transpose-matrix.java
+++ 0898-transpose-matrix/0898-transpose-matrix.java
@@ -3,10 +3,10 @@ class Solution {
         int m=arr.length;
         int n =arr[0].length;
         int[][] result=new int[n][m];
-        
-        for(int i =0;i<m;i++){
-            for(int j=0;j<n;j++){
-                result[j][i]=arr[i][j];
+
+        for(int i =0;i<n;i++){
+            for(int j=0;j<m;j++){
+                result[i][j]=arr[j][i];
 
             }
         }
