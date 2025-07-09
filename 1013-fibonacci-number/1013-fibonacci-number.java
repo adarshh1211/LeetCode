0b26071ba0b6024a4bd73ab122d805a7fa8568cd
adarshh1211 <153364135+adarshh1211@users.noreply.github.com>
2025-07-09 22:18:21 +0530

    Time: 7 ms (45.89%), Space: 40.7 MB (18.35%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,19 +1,10 @@
 class Solution {
     public int fib(int n) {
-         if(n<=0) {
-            return 0;
-         }
-        int first=0;
-        int second=1;
-
-        //if(n==0) return 1;
-
-        for(int i=1;i<n;i++)
-        {
-            int next =first+second;
-            first=second;
-            second=next;
-        }
-        return second;
+        return fibo(n);
+    }
+    public static int fibo(int n) {
+        if (n <= 1)
+            return n;
+        return fibo(n - 1) + fibo(n - 2);
     }
 }
